20ef200e88c88d75f6219daa6047f3ce42394fca
Alp Eren Yalcin <95969634+yalcinalp@users.noreply.github.com>
2022-12-24 18:17:31 +0300

    Add files via upload

---
+++ BinaryHeap.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <vector>
+
+struct HeapElement
+{
+    int     uniqueId;
+    double  weight;
+};
+
+class BinaryHeap
+{
+    private:
+    std::vector<HeapElement> elements;
+
+    // Future tester will utilize this
+    // to access the private parts.
+    friend class HW3Tester;
+
+    protected:
+    public:
+        // Constructors & Destructor
+                            BinaryHeap();
+        //
+        bool                Add(int uniqueId, double weight);
+        bool                PopHeap(int& outUniqueId, double& outWeight);
+        bool                ChangePriority(int uniqueId, double newWeight);
+        int                 HeapSize() const;
+};
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "BinaryHeap.h"
+#include "MeshGraph.h"
+#include "ObjLoader.h"
+
+int main(int argv, const char* argc[])
+{
+    // In order to test the code here
+}
